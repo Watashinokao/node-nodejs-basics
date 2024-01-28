fd2ea985e7ecd4d694ea5f80e30767375faef32e
Darya <watashi.no.kao24@gmail.com>
2024-01-28 17:54:15 +0300

    feat: add fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fsPromise from "fs/promises";
+import { constants } from "fs";
+import path from "path";
+const pathToFile = path.join("fs/files", "fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fsPromise.access(pathToFile, constants.F_OK);
+    console.error("\x1b[31m" + "FS operation failed" + "\x1b[0m");
+  } catch {
+    fsPromise.appendFile(pathToFile, "I am fresh and young");
+  }
 };
 
 await create();
