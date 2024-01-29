1916b926f44c0604097a6e2e87e23c0fd3318e66
Darya <watashi.no.kao24@gmail.com>
2024-01-29 20:14:57 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import fsPromise from "fs/promises";
+import { createHash } from "crypto";
 const calculateHash = async () => {
-    // Write your code here 
+  const data = await fsPromise.readFile(
+    "hash/files/fileToCalculateHashFor.txt",
+    "utf-8"
+  );
+  const hash = createHash("sha256");
+  hash.update(data);
+  console.log(hash.digest("hex"));
 };
 
 await calculateHash();
