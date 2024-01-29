709c4658c12d6052e09b7b9cf690e38b1367e3bf
Darya <watashi.no.kao24@gmail.com>
2024-01-29 18:11:39 +0300

    feat: add fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,11 @@
+import fsPromise from "fs/promises";
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await fsPromise.readdir("fs/files");
+    console.log(files);
+  } catch {
+    console.error("\x1b[31m" + "FS operation failed" + "\x1b[0m");
+  }
 };
 
 await list();
