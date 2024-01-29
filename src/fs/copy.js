93342d2ad57b4a16226c447b0160834a6b213a7b
Darya <watashi.no.kao24@gmail.com>
2024-01-29 14:56:00 +0300

    feat: add fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
-const copy = async () => {
-    // Write your code here 
+import fsPromise from "fs/promises";
+import path from "path";
+const copy = async (pathFolder, pathCopyFolder) => {
+  try {
+    const filesInFolder = await fsPromise.readdir(pathFolder, {
+      withFileTypes: true,
+    });
+    await fsPromise.mkdir(pathCopyFolder);
+    for (const file of filesInFolder) {
+      if (file.isDirectory()) {
+        let newPathFolder = path.join(
+          pathFolder,
+          path.basename(file.name, path.extname(file.name))
+        );
+        let newPathCopyFolder = path.join(
+          pathCopyFolder,
+          path.basename(file.name, path.extname(file.name))
+        );
+        await copy(newPathFolder, newPathCopyFolder);
+      }
+      if (file.isFile()) {
+        await fsPromise.copyFile(
+          path.join(pathFolder, file.name),
+          path.join(pathCopyFolder, file.name)
+        );
+      }
+    }
+  } catch {
+    console.error("\x1b[31m" + "FS operation failed" + "\x1b[0m");
+  }
 };
 
-await copy();
+await copy("fs/files", "fs/files-copy");
