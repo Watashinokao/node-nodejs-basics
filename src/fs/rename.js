f9da7990c3ae465e8efd0e6f0fac9026c20ee6e2
Darya <watashi.no.kao24@gmail.com>
2024-01-29 17:48:07 +0300

    feat: add fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import fsPromise from "fs/promises";
+import path from "path";
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fsPromise.rename(
+      path.join("fs/files", "wrongFilename.txt"),
+      path.join("fs/files", "properFilename.md")
+    );
+  } catch (error) {
+    console.error("\x1b[31m" + "FS operation failed" + "\x1b[0m", error);
+  }
 };
 
 await rename();
